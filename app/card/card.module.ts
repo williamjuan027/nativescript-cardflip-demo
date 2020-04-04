4a8fcd398d978a4acc27c0c7f7396fc58fb6147d
William Juan <williamjuan027@gmail.com>
2020-04-04 16:01:29 -0500

    cleanup

--- app/card/card.module.ts
+++ app/card/card.module.ts
@@ -2,22 +2,21 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptCommonModule } from "nativescript-angular/common";
 import { NativeScriptFormsModule } from "nativescript-angular/forms";
 
-import { CardModule } from "../card/card.module";
-import { HomeRoutingModule } from "./home-routing.module";
-import { HomeComponent } from "./home.component";
+import { CardComponent } from "./card.component";
 
 @NgModule({
     imports: [
         NativeScriptCommonModule,
-        NativeScriptFormsModule,
-        CardModule,
-        HomeRoutingModule,
+        NativeScriptFormsModule
     ],
     declarations: [
-        HomeComponent
+        CardComponent
+    ],
+    exports: [
+      CardComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
     ]
 })
-export class HomeModule { }
+export class CardModule { }
--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,5 +1,4 @@
-import { Component, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
-import { Animation, AnimationDefinition } from "tns-core-modules/ui/animation";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "Home",
@@ -7,93 +6,8 @@ import { Animation, AnimationDefinition } from "tns-core-modules/ui/animation";
     templateUrl: "./home.component.html",
     styleUrls: ["./home.component.css"]
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent {
 
-    @ViewChild('front', { read: ElementRef, static: true }) front: ElementRef;
-    @ViewChild('back', { read: ElementRef, static: true }) back: ElementRef;
+    constructor() {}
 
-    private isFront: boolean = true;
-    private isAnimating: boolean = false;
-
-    constructor() {
-    }
-
-    ngAfterViewInit(): void {
-        this.back.nativeElement.rotateX = 180;
-        this.back.nativeElement.translateY = -50;
-        this.back.nativeElement.opacity = 0;
-    }
-
-    flip(): void {
-        if (!this.isAnimating) {
-            this.isAnimating = true;
-            if (this.isFront) {
-                this.flipToBack().then(() => this.isFront = false);
-            } else {
-                this.flipToFront().then(() => this.isFront = true);
-            }
-        }
-    }
-
-    flipToBack() {
-        const animationDefinition = [
-            {
-                target: this.front.nativeElement,
-                rotate: { x: -180, y: 0, z: 0 },
-                translate: { x: 0, y: -50 },
-                duration: 500
-            },
-            {
-                target: this.back.nativeElement,
-                rotate: { x: 0, y: 0, z: 0 },
-                translate: { x: 0, y: 0 },
-                duration: 500
-            },
-            {
-                target: this.back.nativeElement,
-                opacity: 1,
-                delay: 250,
-                duration: 1
-            },
-            {
-                target: this.front.nativeElement,
-                opacity: 0,
-                delay: 250,
-                duration: 1
-            }
-        ];
-        const animation = new Animation(animationDefinition, false);
-        return animation.play().then(() => this.isAnimating = false).catch(err => console.log('err', err));
-    }
-
-    flipToFront() {
-        const animationDefinition = [
-            {
-                target: this.front.nativeElement,
-                rotate: { x: 0, y: 0, z: 0 },
-                translate: { x: 0, y: 0 },
-                duration: 500
-            },
-            {
-                target: this.back.nativeElement,
-                rotate: { x: 180, y: 0, z: 0 },
-                translate: { x: 0, y: - 50 },
-                duration: 500
-            },
-            {
-                target: this.back.nativeElement,
-                opacity: 0,
-                delay: 250,
-                duration: 1
-            },
-            {
-                target: this.front.nativeElement,
-                opacity: 1,
-                delay: 250,
-                duration: 1
-            }
-        ];
-        const animation = new Animation(animationDefinition, false);
-        return animation.play().then(() => this.isAnimating = false).catch(err => console.log('err', err));
-    }
 }
